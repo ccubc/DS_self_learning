e2a7d5761176017927a55d2b8f1bc492f40290b2
ccubc <chengchen1215@gmail.com>
2019-11-03 21:27:50 -0500

    best time to buy and sell stock II

---
+++ leetcode_algorithm/leetcode_easy_122.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  3 21:08:03 2019
+
+@author: chengchen
+
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+
+Example 1:
+
+Input: [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+             Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Example 2:
+
+Input: [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+             Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
+             engaging multiple transactions at the same time. You must sell before buying again.
+Example 3:
+
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+
+"""
+
+def maxProfit(prices):
+    if (len(prices)<2):
+        return 0
+    else:
+        profit = 0
+        for i in range(1,len(prices)):
+            profit += max(prices[i]-prices[i-1], 0)
+        return profit
+print(maxProfit([7,1,5,3,6,4]))
+print(maxProfit([1,2,3,4,5]))
+print(maxProfit([7,6,4,3,1]))
