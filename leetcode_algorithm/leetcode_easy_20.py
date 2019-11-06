ee2f1dcb9f3e9a54fc1565b903178753b9658358
ccubc <chengchen1215@gmail.com>
2019-11-05 22:59:50 -0500

    valid parentheses

---
+++ leetcode_algorithm/leetcode_easy_20.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  5 22:29:09 2019
+
+@author: chengchen
+
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+Example 1:
+
+Input: "()"
+Output: true
+Example 2:
+
+Input: "()[]{}"
+Output: true
+Example 3:
+
+Input: "(]"
+Output: false
+Example 4:
+
+Input: "([)]"
+Output: false
+Example 5:
+
+Input: "{[]}"
+Output: true
+"""
+s = "{{[]}}"
+def isValid(s):
+    list_s = list(s)
+    dic = {'{':'}',
+       '[':']',
+       '(':')'}
+    stack = []
+    flag = True
+    for i in range(len(list_s)):
+        if list_s[i] in dic.keys():
+            stack.append(dic[list_s[i]])
+        else:
+            if len(stack)==0:
+                return False
+            else:
+                if (list_s[i] != stack.pop()):
+                    return False
+    if len(stack)>0:
+        return False
+    return flag
+print(isValid(s))
+        
+    
