ea94737654f91ad8a08292c824e485924d75acff
ccubc <chengchen1215@gmail.com>
2019-11-09 19:00:19 -0500

    quick sort (including in-place algorithm)

---
+++ leetcode_algorithm/algorithm_quick_sort.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  9 16:16:07 2019
+
+@author: chengchen
+"""
+
+#%%
+def quickSort(x):
+    if len(x) <= 1:
+        return x
+    else:
+        pivot = x[0]
+        left_list = []
+        right_list = []
+        for i in x[1:]:
+            if i <= pivot:
+                left_list.append(i)
+            else:
+                right_list.append(i)
+        return quickSort(left_list)+[pivot]+quickSort(right_list)
+print(quickSort([3,1,2,6,2]))
+
+
+#%% sort in place
+def quickSort(x):
+    if len(x) <= 1:
+        return x
+    else:
+        x, pivot = partition(x)
+        return quickSort(x[:pivot]) + [x[pivot]] + quickSort(x[pivot+1:])
+
+def partition(x):
+    pivot = 0
+    right_p = len(x) - 1
+    pt = 1
+    while pt <= right_p:
+        if x[pt]<=x[pivot]:
+            x[pt], x[pivot] = x[pivot], x[pt]
+            pivot += 1
+            pt += 1
+        else:
+            x[pt], x[right_p] = x[right_p], x[pt]
+            right_p -= 1
+    return x, pivot
+
+print(quickSort([3,1,4,2,5]))
+
+
