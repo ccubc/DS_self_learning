346a2a323dcd6906bee7fe5df752e56fda8dd789
ccubc <chengchen1215@gmail.com>
2019-11-30 18:58:51 -0500

    max depth of n-aray tree

--- leetcode_algorithm/leetcode_easy_559.py
+++ leetcode_algorithm/leetcode_easy_559.py
@@ -9,7 +9,7 @@ Given a n-ary tree, find its maximum depth.
 
 The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 """
-
+ 
 """
 # Definition for a Node.
 class Node:
