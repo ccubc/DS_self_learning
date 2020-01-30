c9694268c3114eeba3083e5a3b2fc579425e9c7b
ccubc <chengchen1215@gmail.com>
2020-01-29 21:52:06 -0500

    i am happy today and i hope jerry gets an offer soon

---
+++ leetcode_algorithm/leetcode_easy_101.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 29 21:49:41 2020
+
+@author: chengchen
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+  def isSymmetric(self, root):
+    if root is None:
+      return True
+    else:
+      return self.isMirror(root.left, root.right)
+
+  def isMirror(self, left, right):
+    if left is None and right is None:
+      return True
+    if left is None or right is None:
+      return False
+
+    if left.val == right.val:
+      outPair = self.isMirror(left.left, right.right)
+      inPiar = self.isMirror(left.right, right.left)
+      return outPair and inPiar
+    else:
+      return False
+        
