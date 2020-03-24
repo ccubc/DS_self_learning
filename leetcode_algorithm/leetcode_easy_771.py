967b8c6c04fdcd45a236f979107312d37a6154c9
ccubc <chengchen1215@gmail.com>
2020-03-23 22:13:20 -0400

    jewels and stones, using Counter object

---
+++ leetcode_algorithm/leetcode_easy_771.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 23 22:12:37 2020
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: J = "z", S = "ZZ"
+Output: 0
+@author: chengchen
+"""
+
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        c = collections.Counter(S)
+        return sum([c[n] for n in J])
