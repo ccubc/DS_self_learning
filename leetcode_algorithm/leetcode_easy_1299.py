ca214bee755615e6b5ab1827cc13e29021db2468
ccubc <chengchen1215@gmail.com>
2020-03-28 21:40:35 -0400

    replace elements with greatest element to the right side

---
+++ leetcode_algorithm/leetcode_easy_1299.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 28 21:39:49 2020
+Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+
+After doing so, return the array.
+@author: chengchen
+"""
+
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        cur_m = -1
+        ans = [-1]
+        for i in range(len(arr)-1, 0, -1):
+            cur_m = max(cur_m, arr[i])
+            ans = [cur_m] + ans
+        return ans
