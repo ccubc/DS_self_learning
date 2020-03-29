155eb3ba5111554e407cb2043d332e5782335c56
ccubc <chengchen1215@gmail.com>
2020-03-28 21:55:19 -0400

    a faster solution to 1299

--- leetcode_algorithm/leetcode_easy_1299.py
+++ leetcode_algorithm/leetcode_easy_1299.py
@@ -9,6 +9,16 @@ After doing so, return the array.
 """
 
 class Solution:
+# speed is much faster hahahahaha
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        pre_m = -1
+        for i in range(len(arr)-1, 0, -1):
+            cur_m = max(pre_m, arr[i])
+            arr[i] = pre_m
+            pre_m = cur_m
+        arr[0] = pre_m
+        return arr    
+    
 # speed is faster
     def replaceElements(self, arr: List[int]) -> List[int]:
         cur_m = -1
