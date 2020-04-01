c4c9e2504d4660027966f061b3c6cbcfd3162717
ccubc <chengchen1215@gmail.com>
2020-03-31 20:33:09 -0400

    count negative numbers in a sorted matrix

--- leetcode_algorithm/leetcode_easy_1351.py
+++ leetcode_algorithm/leetcode_easy_1351.py
@@ -7,3 +7,16 @@ class Solution:
                 i += 1
             res += (len(row) - i)
         return res
+
+
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        res = 0
+        n = len(grid[0])
+        for row in grid:
+            i = 0
+            for i in range(n):
+                if row[i] < 0:
+                    res += (n - i)
+                    break
+        return res
