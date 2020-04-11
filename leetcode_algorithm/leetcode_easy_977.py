67c506ecd2cfd2bf7364c3495f465e40fc9dd3b4
ccubc <chengchen1215@gmail.com>
2020-04-10 20:41:16 -0400

    added solution using collection.deque

--- leetcode_algorithm/leetcode_easy_977.py
+++ leetcode_algorithm/leetcode_easy_977.py
@@ -10,6 +10,26 @@ Output: [0,1,9,16,100]
 """
 
 
+# O(N) solution:
+from collections import deque
+class Solution:
+    def sortedSquares(self, A: List[int]) -> List[int]:
+        l = 0
+        r = len(A) - 1
+        res = deque()
+        while (l <= r):
+            if abs(A[l]) < abs(A[r]):
+                res.appendleft(A[r]**2)
+                r -= 1
+            else:
+                res.appendleft(A[l]**2)
+                l += 1
+        return res
+
+
+
+# one liner, O(NlogN) solution
 class Solution:
     def sortedSquares(self, A: List[int]) -> List[int]:
         return sorted([i**2 for i in A])
+
