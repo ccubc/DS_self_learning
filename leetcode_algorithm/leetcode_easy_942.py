341a81d6af02747eaeef8945e6f24baa38bb04e2
ccubc <chengchen1215@gmail.com>
2020-04-12 21:06:14 -0400

    DI string match - added faster solution

--- leetcode_algorithm/leetcode_easy_942.py
+++ leetcode_algorithm/leetcode_easy_942.py
@@ -13,6 +13,18 @@ Input: "IDID"
 Output: [0,4,1,3,2]
 """
 
+class Solution:
+    def diStringMatch(self, S: str) -> List[int]:
+        low, high = 0, len(S)
+        ans = []
+        for c in S:
+            if c == "I":
+                ans.append(low)
+                low += 1
+            else:
+                ans.append(high)
+                high -= 1
+        return ans + [low]
 
 
 
