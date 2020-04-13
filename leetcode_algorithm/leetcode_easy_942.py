a07e2917d6b262229a9814c500c277100e8e187c
ccubc <chengchen1215@gmail.com>
2020-04-12 20:58:25 -0400

    DI string match

---
+++ leetcode_algorithm/leetcode_easy_942.py
@@ -0,0 +1,36 @@
+"""
+Given a string S that only contains "I" (increase) or "D" (decrease), let N = S.length.
+
+Return any permutation A of [0, 1, ..., N] such that for all i = 0, ..., N-1:
+
+If S[i] == "I", then A[i] < A[i+1]
+If S[i] == "D", then A[i] > A[i+1]
+ 
+
+Example 1:
+
+Input: "IDID"
+Output: [0,4,1,3,2]
+"""
+
+
+
+
+import numpy as np
+class Solution:
+    def diStringMatch(self, S: str) -> List[int]:
+        level = 0
+        level_list = [level]
+        mapping = {"I": 1,
+                  "D": -1}
+        for i, c in enumerate(S):
+            level += mapping[c]
+            level_list.append(level)
+        pos = np.argsort(level_list)
+        res = [1]*len(level_list)
+        a = 0
+        for i in pos:
+            res[i] = a
+            a += 1
+        return res
+        
