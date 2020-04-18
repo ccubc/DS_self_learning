6146e9ac09314b947b25df7e1799b2886597e311
ccubc <chengchen1215@gmail.com>
2020-04-17 20:05:09 -0400

    number of recent calls --- class basics

---
+++ leetcode_algorithm/leetcode_easy_933.py
@@ -0,0 +1,34 @@
+"""
+Write a class RecentCounter to count recent requests.
+
+It has only one method: ping(int t), where t represents some time in milliseconds.
+
+Return the number of pings that have been made from 3000 milliseconds ago until now.
+
+Any ping with time in [t - 3000, t] will count, including the current ping.
+
+It is guaranteed that every call to ping uses a strictly larger value of t than before.
+
+ 
+
+Example 1:
+
+Input: inputs = ["RecentCounter","ping","ping","ping","ping"], inputs = [[],[1],[100],[3001],[3002]]
+Output: [null,1,2,3,3]
+"""
+
+class RecentCounter:
+
+    def __init__(self):
+        self.queue = collections.deque()
+
+    def ping(self, t: int) -> int:
+        self.queue.append(t)
+        while self.queue[0] < t-3000:
+            self.queue.popleft()
+        return len(self.queue)
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
