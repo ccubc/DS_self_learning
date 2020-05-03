8f395b2a597970499a299b4ac0638ecc07c120cc
ccubc <chengchen1215@gmail.com>
2020-05-03 19:16:43 -0400

    minor update

--- leetcode_algorithm/leetcode_easy_1047.py
+++ leetcode_algorithm/leetcode_easy_1047.py
@@ -15,9 +15,6 @@ Explanation:
 For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
 """
 
-
-
-
 class Solution:
     def removeDuplicates(self, S: str) -> str:
         stack = []
