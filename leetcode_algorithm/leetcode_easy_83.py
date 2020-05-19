0961c5d48d2d8dbff4812e6e0bd037eee517f4bc
ccubc <chengchen1215@gmail.com>
2020-05-18 21:05:42 -0400

    remove duplicates from sorted linkedlist

--- leetcode_algorithm/leetcode_easy_83.py
+++ leetcode_algorithm/leetcode_easy_83.py
@@ -15,11 +15,27 @@ Output: 1->2->3
 
 
 
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        cur = head
+        if not head:
+            return head
+        while cur.next:
+            if cur.val != cur.next.val:
+                cur = cur.next
+            else:
+                cur.next = cur.next.next
+        return head
+
+
+
 class Solution:
     def deleteDuplicates(self, head: ListNode) -> ListNode:
         cur = head
