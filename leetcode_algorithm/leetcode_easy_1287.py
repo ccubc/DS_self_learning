e6fdd77a808bb688c07619742d80409200d2174d
ccubc <chengchen1215@gmail.com>
2020-05-31 19:34:30 -0400

    element appearing more than 25% in sorted array

---
+++ leetcode_algorithm/leetcode_easy_1287.py
@@ -0,0 +1,26 @@
+"""
+Given an integer array sorted in non-decreasing order, there is exactly one integer in the array that occurs more than 25% of the time.
+
+Return that integer.
+
+ 
+
+Example 1:
+
+Input: arr = [1,2,2,6,6,6,6,7,10]
+Output: 6
+ 
+
+Constraints:
+
+1 <= arr.length <= 10^4
+0 <= arr[i] <= 10^5
+"""
+
+
+class Solution:
+    def findSpecialInteger(self, arr: List[int]) -> int:
+        t = len(arr)//4
+        for i, n in enumerate(arr):
+            if arr[i] == arr[i+t]:
+                return n
