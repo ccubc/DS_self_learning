ef8b9d99d909b0f28e7352c2f290f94edf107b9a
ccubc <chengchen1215@gmail.com>
2020-06-23 21:27:45 -0400

    swap nodes in pairs--- linkedlist, recursion

---
+++ leetcode_algorithm/leetcode_medium_24.py
@@ -0,0 +1,31 @@
+"""
+Given a linked list, swap every two adjacent nodes and return its head.
+
+You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+ 
+
+Example:
+
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        tmp2 = head.next.next
+        tmp2 = self.swapPairs(tmp2)
+        tmp1 = head
+        head = head.next
+        head.next = tmp1
+        tmp1.next = tmp2
+        return head
+        
+        
