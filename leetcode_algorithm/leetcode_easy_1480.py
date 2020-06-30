edaf7190ea7fcb8a82e4d6023efe86576ce6dd51
ccubc <chengchen1215@gmail.com>
2020-06-29 20:08:53 -0400

    running sum of 1d array

--- leetcode_algorithm/leetcode_easy_1480.py
+++ leetcode_algorithm/leetcode_easy_1480.py
@@ -30,5 +30,5 @@ Constraints:
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         for i in range(1, len(nums)):
-            nums[i] = nums[i]+nums[i-1]
+            nums[i] += nums[i-1]
         return nums
