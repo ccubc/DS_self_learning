b8c3a6cab796dc69cdf826cf32b304db761fa136
ccubc <chengchen1215@gmail.com>
2020-07-17 23:46:53 -0400

    更简洁的invert binary tree

--- leetcode_algorithm/leetcode_easy_226.py
+++ leetcode_algorithm/leetcode_easy_226.py
@@ -46,3 +46,9 @@ class Solution:
             root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
             return root
         
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root:
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        return root
