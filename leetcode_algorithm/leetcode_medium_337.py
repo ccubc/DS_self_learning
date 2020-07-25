e60e42ff8013dd80491d722cef99e3456b48112c
ccubc <chengchen1215@gmail.com>
2020-07-25 00:06:30 -0400

    recursion, 间隔遍历一棵树 house robber III

---
+++ leetcode_algorithm/leetcode_medium_337.py
@@ -0,0 +1,49 @@
+"""
+The thief has found himself a new place for his thievery again. There is only one entrance to this area, called the "root." Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that "all houses in this place forms a binary tree". It will automatically contact the police if two directly-linked houses were broken into on the same night.
+
+Determine the maximum amount of money the thief can rob tonight without alerting the police.
+
+Example 1:
+
+Input: [3,2,3,null,3,null,1]
+
+     3
+    / \
+   2   3
+    \   \ 
+     3   1
+
+Output: 7 
+Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+Example 2:
+
+Input: [3,4,5,1,3,null,1]
+
+     3
+    / \
+   4   5
+  / \   \ 
+ 1   3   1
+
+Output: 9
+Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rob(self, root: TreeNode) -> int:
+        if not root: 
+            return 0
+        rob_root_level = root.val
+        if root.left:
+            rob_root_level += (self.rob(root.left.left) + self.rob(root.left.right))
+        if root.right:
+            rob_root_level += (self.rob(root.right.left) + self.rob(root.right.right))
+        rob_child_level = self.rob(root.left) + self.rob(root.right)
+        return max(rob_root_level, rob_child_level)
