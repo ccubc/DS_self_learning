dba328b335677a86a33b70851f98b0c64726c93a
ccubc <chengchen1215@gmail.com>
2020-08-16 19:42:29 -0400

    valid anagram

---
+++ leetcode_algorithm/leetcode_easy_242.py
@@ -0,0 +1,15 @@
+"""
+Given two strings s and t , write a function to determine if t is an anagram of s.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+"""
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s)==sorted(t)
