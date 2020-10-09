25264d9399069d0b2565c526d61eee8744d2ed3d
ccubc <chengchen1215@gmail.com>
2020-10-08 22:59:58 -0400

    more on 继承

--- practical_python/OOP/3_inheritence.py
+++ practical_python/OOP/3_inheritence.py
@@ -24,9 +24,20 @@ class Pet:
 	def show(self):
 		print(f"I am {self.name} and I am {self.age} years old")
 
+	def speak(self):
+		print("I don't know what to say")
+
+
 class Cat(Pet):
+	def __init__(self, name, age, color):
+		super().__init__(name, age) # super references the parent class
+		self.color = color
+
 	def speak(self):
 		print("Meow " * 3)
+
+	def show(self):
+		print(f"I am {self.name} and I am {self.age} years old and I am {self.color}")
 
 class Dog(Pet):
 	def speak(self):
@@ -35,7 +46,7 @@ class Dog(Pet):
 p = Pet("Jerry", 7)
 p.show()
 
-c = Cat("Meowda", 8)
+c = Cat("Meowda", 8, "white")
 c.show()
 c.speak()
 
