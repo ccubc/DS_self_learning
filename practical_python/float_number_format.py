1d7de70b03aab2ecd8507098a49bbacf85c5b653
ccubc <chengchen1215@gmail.com>
2020-10-09 21:54:51 -0400

    float number format

---
+++ practical_python/float_number_format.py
@@ -0,0 +1,6 @@
+pi = 3.1415926
+print("{:.2f}".format(pi)) # 3.14
+print("{:,.2f}".format(pi*1000000)) # 3,141,592.60
+print("${:,.2f}".format(pi*1000000)) # $3,141,592.60
+print("{:.2g}".format(pi*1000000)) # 3.1e+06
+print("{:.2e}".format(pi*1000000)) # 3.14e+06
