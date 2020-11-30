63437a81c2b409c29dab1469f40d055627aae08c
ccubc <chengchen1215@gmail.com>
2020-11-29 23:42:56 -0500

    一道有点弱智的题

---
+++ leetcode_algorithm/leetcode_easy_551.py
@@ -0,0 +1,31 @@
+"""
+551. Student Attendance Record I
+Easy
+
+327
+
+884
+
+Add to List
+
+Share
+You are given a string representing an attendance record for a student. The record only contains the following three characters:
+'A' : Absent.
+'L' : Late.
+'P' : Present.
+A student could be rewarded if his attendance record doesn't contain more than one 'A' (absent) or more than two continuous 'L' (late).
+
+You need to return whether the student could be rewarded according to his attendance record.
+
+Example 1:
+Input: "PPALLP"
+Output: True
+Example 2:
+Input: "PPALLL"
+Output: False
+"""
+
+class Solution:
+    def checkRecord(self, s: str) -> bool:
+        return not(collections.Counter(s)['A']>1 or ('LLL' in s))
+            
