d3bee24def882cd61c97f4b2e86f8b076aa2a684
ccubc <chengchen1215@gmail.com>
2021-01-13 00:59:19 -0500

    the most frequently used print format float

--- practical_python/float_number_format.py
+++ practical_python/float_number_format.py
@@ -4,3 +4,4 @@ print("{:,.2f}".format(pi*1000000)) # 3,141,592.60
 print("${:,.2f}".format(pi*1000000)) # $3,141,592.60
 print("{:.2g}".format(pi*1000000)) # 3.1e+06
 print("{:.2e}".format(pi*1000000)) # 3.14e+06
+print(f"{pi:.2f}") # 3.14
